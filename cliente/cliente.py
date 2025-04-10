12a5e3eee8020a7217533f7c808728d58e57f9d8
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-04-10 16:40:27 -0300

    fix: use valid host

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,9 +1,6 @@
 import requests
-import time
-
-time.sleep(5)
 
 cpf = input("Digite um CPF para validar: ")
-res = requests.post("http://servidor:5000/validar", json={"cpf": cpf})
+res = requests.post("http://localhost:5000/validar", json={"cpf": cpf})
 
 print("Resposta do servidor:", res.json())
